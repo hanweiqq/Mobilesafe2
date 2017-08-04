38e63313c16752d849066c1334c74c09a65faa86
hanV <hanweiqq163@.com>
2017-08-04 14:48:42 +0800

    设置向导的第一个页面

--- app/src/main/java/com/heimad/mobilesafe/ui/SetupActivity1.java
+++ app/src/main/java/com/heimad/mobilesafe/ui/SetupActivity1.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.widget.TextView;
 
+import com.heimad.mobilesafe.R;
+
 /**
  * Created by Administrator on 2017/8/4.
  */
@@ -14,9 +16,6 @@ public class SetupActivity1 extends Activity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        TextView tv = new TextView(getApplicationContext());
-        tv.setTextColor(Color.WHITE);
-        tv.setText("这是手机设置向导页面");
-        setContentView(tv);
+        setContentView(R.layout.activity_setup1);
     }
 }
